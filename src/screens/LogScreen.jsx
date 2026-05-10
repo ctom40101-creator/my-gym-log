ae1c7caae2a66e7b9a266d72ae3bcfb75aba426f
ctom40101 <ctom40101@gmail.com>
2026-05-11 00:07:03 +0800

    extract overview screen

--- src/screens/LogScreen.jsx
+++ src/screens/LogScreen.jsx
@@ -2,12 +2,24 @@
 // LogScreen.jsx
 // 訓練紀錄頁面
 
+// V1.0
+// LogScreen.jsx
+// 訓練紀錄主頁
+
 import React from 'react';
 
-function LogScreen() {
+function LogScreen({
+  logDB,
+  setLogDB,
+  movementDB,
+  bodyMetricsDB,
+  plansDB,
+  user,
+}) {
+
   return (
     <div>
-      Log Screen
+      {/* Log Screen 開發中 */}
     </div>
   );
 }
